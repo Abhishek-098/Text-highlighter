9e253900783abc1125b3170d887f3b70c1e49b1b
Abhishek Pratap Singh Chauhan <chauhanabhi0098@gmail.com>
2021-04-09 12:22:32 +0530

    Completed

--- src/Text.jsx
+++ src/Text.jsx
@@ -4,15 +4,23 @@ import './CSS/text.css';
 
 function removeItem(array, item){
     for(var i in array){
-        if(array[i]===item){
+        if(array[i].comVal===item){
             array.splice(i,1);
             break;
         }
     }
+    for(var j in array){
+        array[j].cno-=1;
+    }
 }
-
+var count=0;
 const Text =(props) =>{
-
+    const [objects,addObject] =useState({
+        comVal:"",
+        // title:"",
+        date:"",
+        cno:0,
+    });
     const [comments,addComment] = useState([]);
     const handleMouseUp =(event)=>{
         const text = window.getSelection().toString();
@@ -22,43 +30,69 @@ const Text =(props) =>{
             const y = event.pageY;
             document.querySelector('#btnenable').style.left = `${x}px`;
             document.querySelector('#btnenable').style.top = `${y}px`;
+            addObject(()=>{
+                count+=1
+                var x = new Date();
+                var h = x.toLocaleTimeString();
+                return{
+                    comVal:text,
+                    date: h,
+                    cno:count,
+                };
+            });
 
         }
         
     }
     const commentEvent =()=>{
-        const text = window.getSelection().toString();
-        addComment(oldarr=>[...oldarr,text]);   
+        addComment(oldarr=>[...oldarr,objects]);  
+        addObject(()=>{
+            return{
+                comVal:"",
+                date:"",
+            }
+        })
         document.querySelector('#btnenable').style.display = "None";
         
 
     }
+    const getArr=()=>{
+        const c = [];
+        for(var i =0;i<comments.length;i++){
+            c.push(comments[i].comVal);
+        }
+        return c;
+    }
+    const arrofComm = getArr()
+
+    
     
     // eslint-disable-next-line array-callback-return
     const ndata =comments.map((value,ind)=>{
         const cDelete=()=>{
             addComment(()=>{
-                removeItem(comments,value);
+                removeItem(comments,value.comVal);
+
                 return comments;
+
             })
             commentEvent();
-            
-
+            count-=1;
     
         }
         
 
-        if(value===""){
+        if(value.comVal===""){
             console.log("Comment deleted");
         }else{
 
         return(
             <div className="card my-2" key={ind}>
                                 <div className="card-body">
-                                    <h6>{`COMMENT:${ind+1}`}</h6>
-                                    <p className="card-text">{value}</p>
-                                    {/* <a href="#" className="btn btn-primary">Go somewhere</a> */}
+                                    <h6>{`COMMENT:${value.cno}`}</h6>
+                                    <p className="card-text">{value.comVal}</p>
                                     <button className = " delete_comment btn btn-danger" onClick ={cDelete}>Delete</button>
+                                    <span className= "time">{value.date}</span>
                                 </div>
                             </div>
         );}
@@ -71,7 +105,7 @@ const Text =(props) =>{
                                     <div className ='js_selected_text'>
                                         <Highlighter 
                                         highlightClassName="YourHighlightClass"
-                                            searchWords={comments}
+                                            searchWords={arrofComm}
                                             autoEscape={true}
                                             textToHighlight= {props.val}
                                             onMouseUp ={handleMouseUp}
